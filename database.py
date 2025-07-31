6ff7fa4927d3474c1620de995aae42bf64d0e479
Carlos Eduardo <cadualmeida663@gmail.com>
2025-07-31 09:44:50 -0300

    Versão final estável com todas as funcionalidades

--- database.py
+++ database.py
@@ -2,21 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-# Em database.py
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ADICIONE A URL EXTERNA DO BANCO DE DADOS DA RENDER DIRETAMENTE
-DATABASE_URL = "postgresql://auditoria_py_user:3nua3J3TPFe1aPs0mlFiPJXL1n20WE3J@dpg-d25l6mc9c44c73dg5m20-a.oregon-postgres.render.com/auditoria_py"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 load_dotenv()
 
 # Lógica para funcionar tanto online (Render) quanto localmente
-DATABASE_URL = os.getenv("DATABASE_URL") # Render define esta variável de ambiente
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
 if not DATABASE_URL:
     # Se não estiver na Render, usa as variáveis do .env para conectar localmente
