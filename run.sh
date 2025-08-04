752067681a06796ac58c1b4e6980e05bec94b1e4
Carlos Eduardo <cadualmeida663@gmail.com>
2025-08-04 09:16:34 -0300

    Ajusta comando de inicialização do Uvicorn para Render

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-#!/usr/-bin/env sh
-
-# Adiciona a opção --workers 1 para melhor estabilidade
-uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
+#!/usr/bin/env sh
+uvicorn main:app --host 0.0.0.0 --port 10000 --workers 1
